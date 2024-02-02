ff91a7025275f7dadf3626a33a93b3d57661be27
Chris Freckleton <36802104+TheFreck@users.noreply.github.com>
2024-02-01 19:02:23 -0700

    Works for non-negative coefficients

--- CodeKataBinomialExpansion.Specs/BinomialExpansion.Spec.cs
+++ CodeKataBinomialExpansion.Specs/BinomialExpansion.Spec.cs
@@ -7,7 +7,7 @@ namespace CodeKataBinomialExpansion.Specs
         Establish context = () =>
         {
             inputs = new string[]{
-                "(x-1)^0",
+                "(2x+3)^0",
                 "(x-1)^1",
                 "(x-1)^2"
             };
@@ -15,7 +15,7 @@ namespace CodeKataBinomialExpansion.Specs
             {
                 new decimal[]
                 {
-                    1,-1,0
+                    2,3,0
                 },
                 new decimal[]
                 {
@@ -56,22 +56,117 @@ namespace CodeKataBinomialExpansion.Specs
         Establish context = () =>
         {
             inputs = new string[]{
+                "(x+1)^2",
                 "(x-1)^0",
                 "(x-1)^1",
-                "(x-1)^2"
+                "(x-1)^2",
             };
             expected = new string[]
             {
+                "x^2+2x+1",
                 "1",
                 "x-1",
-                "x^2-2x+1"
+                "x^2-2x+1",
             };
             answers = new string[inputs.Length];
         };
 
+        Because of = () =>
+        {
+            for(var i=0; i < inputs.Length; i++)
+            {
+                answers[i] = Expander.Expand(inputs[i]);
+            }
+        };
+
+        It Should_Return_Values_In_A_String_Array = () => answers.ShouldEqual(expected);
+        It Should_Return_An_Answer_For_Each_Input = () =>
+        {
+            for (var i = 0; i < answers.Length; i++)
+            {
+                answers[i].ShouldEqual(expected[i]);
+            }
+        };
 
         private static string[] inputs;
         private static string[] expected;
         private static string[] answers;
+    }
+
+    public class When_Finding_A_Coefficient
+    {
+        Establish context = () =>
+        {
+            input = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            expect = new int[][] 
+            {
+                new int[] { 1 },
+                new int[] { 1,1 },
+                new int[] { 1,2,1 },
+                new int[] { 1,3,3,1},
+                new int[] { 1,4,6,4,1},
+                new int[] { 1,5,10,10,5,1},
+                new int[] { 1,6,15,20,15,6,1},
+                new int[] { 1,7,21,35,35,21,7,1},
+                new int[] { 1,8,28,56,70,56,28,8,1}
+            };
+            answers = new int[input.Length][];
+        };
+
+        Because of = () =>
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                answers[i] = Expander.FindCoefficients(input[i]);
+            }
+        };
+
+        //It Should_Return_Correct_Results_In_An_Int_Array = () => answers.ShouldEqual(expect);
+        It Should_Return_Expected_Results = () =>
+        {
+            for (var i = 0; i < answers.Length; i++)
+            {
+                for(var j=0; j < answers[i].Length; j++)
+                {
+                    var answer = answers[i][j];
+                    var expected = expect[i][j];
+                    answers[i][j].ShouldEqual(expect[i][j]);
+                }
+            }
+        };
+
+        private static int[] input;
+        private static int[][] expect;
+        private static int[][] answers;
+    }
+
+    public class When_Calculating_A_Factorial
+    {
+        Establish context = () =>
+        {
+            input = new int[] { 2, 3, 4, 5, 6, 7, 8, 9 };
+            expect = new int[] { 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+            answers = new int[input.Length];
+        };
+
+        Because of = () =>
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                answers[i] = Expander.Factorialize(input[i]);
+            }
+        };
+
+        It Should_Return_Correct_Answer = () =>
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                answers[i].ShouldEqual(expect[i]);
+            }
+        };
+
+        private static int[] input;
+        private static int[] expect;
+        private static int[] answers;
     }
 }
--- CodeKataBinomialExpansion/Expander.cs
+++ CodeKataBinomialExpansion/Expander.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CodeKataBinomialExpansion
@@ -10,28 +11,55 @@ namespace CodeKataBinomialExpansion
     {
         public static string Expand(string expr)
         {
-            return "";
+            var variables = Parse(expr);
+            var x = variables[0];
+            var y = variables[1];
+            var n = variables[2];
+            if (variables.LastOrDefault() == 0) return "1";
+            var coefficients = FindCoefficients((int)n);
+            var output = String.Empty;
+            for(var i=0; i<=n; i++)
+            {
+                var sign = coefficients[i] < 0 ? "-" : "+";
+                var coefficient = (coefficients[i] != 1 && n-i != 0) ? coefficients[i].ToString() : n-i==0 ? "1" : "";
+                var variable = (coefficients[i] != 0 && n-i > 1) ? "x^" : (coefficients[i] != 0 && n-i > 0) ? "x" : "";
+                var exponent = (coefficients[i] != 0 && n-i > 1) ? (n - i).ToString() : "";
+                output += $"{sign}{coefficient}{variable}{exponent}";
+            }
+            return output.Trim('+');
+        }
+
+        public static int Factorialize(int f)
+        {
+            var fFact = 1;
+            for(var i=2; i<=f; i++)
+            {
+                fFact *= i;
+            }
+            return fFact;
+        }
+
+        public static int[] FindCoefficients(int n)
+        {
+            var nFact = Factorialize(n);
+            var coefficients = new int[n+1];
+            for(var k=0; k <= n; k++)
+            {
+                var kFact = Factorialize(k);
+                var nkFact = Factorialize(n-k);
+                if (kFact * nkFact == 0) coefficients[k] = 1;
+                else coefficients[k] = nFact / (kFact * nkFact);
+            }
+            return coefficients;
         }
 
         public static decimal[] Parse(string v)
         {
-            var x = 0;
-            var y = 0;
-            var n = 0;
-            for(var i=0; i<v.Length; i++)
-            {
-                switch(v[i])
-                {
-                    case '(':
-                        break;
-                    case ')':
-                        break;
-                    case '^':
-                        break;
-                    default:
-                        break;
-                }
-            }
+            var x = Regex.Match(v, "(?<=\\()(.*)(?=x)");
+            var y = Regex.Match(v, "(?<=x)(.*)(?=\\))");
+            var n = Regex.Match(v, "[^^]*$");
+            
+            return new decimal[] { Decimal.Parse(x.Value != "" ? x.Value : "1"), Decimal.Parse(y.Value != "" ? y.Value : "0"), Decimal.Parse(n.Value != "" ? n.Value : "0") };
         }
     }
 
